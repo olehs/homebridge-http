b219946f46d523a686c17fe31c79aa4daa9df55b
olehs <olehs@i.ua>
2016-08-09 23:15:24 +0300

    Update index.js
    on/off values

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function HttpExtAccessory(log, config) {
 
 function stringToBoolean(string){
     switch(string.toLowerCase().trim()){
-        case "true": case "yes": case "1": return true;
-        case "false": case "no": case "0": case null: return false;
+        case "true": case "yes": case "on": case "1": return true;
+        case "false": case "no": case "off": case "0": case null: return false;
         default: return Boolean(string);
     }
 }
